505f5a1f1e02333c1b20374b408e13b1ad426c59
Richard Gill <richard@rgill.co.uk>
2021-12-06 08:13:22 +0000

    working

---
+++ src/days/06.1/index.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+import { readRelativeInput } from '@/common/file.js';
+
+const readInput = (fileName) => readRelativeInput(import.meta.url, fileName);
+
+const parseInput = (input) => input.trim().split(',').map((n) => parseInt(n, 10));
+
+const runDay = (timers) => {
+  const originalTimers = timers.map((t) => {
+    if (t === 0) {
+      return 6;
+    }
+    return t - 1;
+  });
+  const newFishCount = _.filter(timers, (t) => t === 0).length;
+  return [...originalTimers, ..._.times(newFishCount, () => 8)];
+};
+
+const runDays = (timers, days) => {
+  _.times(days, (day) => {
+    timers = runDay(timers);
+    console.log(`${day + 1} days: ${timers.join(',')}, count: ${timers.length}`);
+  });
+  return timers;
+};
+
+export const solve = (input) => {
+  const timers = parseInput(input);
+  console.log('timers', timers.join(','));
+  return runDays(timers, 40).length;
+};
+
+console.log(solve('4'), '\n\n\n');
+// console.log(solve(readInput('example1.txt')), '\n\n\n');
+// console.log(solve(readInput('example2.txt')), '\n\n\n');
+// console.log(solve(readInput('puzzleInput.txt')), '\n\n\n');
