382811505135f80062eaf8efdbbbe07d5c480a98
Richard Gill <richard@rgill.co.uk>
2021-12-16 08:10:11 +0000

    day 16 done

--- src/days/16.2/index.js
+++ src/days/16.2/index.js
@@ -12,49 +12,14 @@ const GREATER_THAN = 5;
 const LESS_THAN = 6;
 const EQUAL_TO = 7;
 
+const LENGTH_TYPE_0_LENGTH = 15;
+const LENGTH_TYPE_1_LENGTH = 11;
+const LENGTH_TYPE_HEADER_LENGTH = 7;
+const LITERAL_GROUP_LENGTH = 5;
+const LITERAL_HEADER_LENGTH = 6;
+
 const hexDigitToBinary = (hexDigit) => {
-  switch (hexDigit) {
-    case '0':
-      return '0000';
-    case '1':
-      return '0001';
-    case '2':
-      return '0010';
-    case '3':
-      return '0011';
-    case '4':
-      return '0100';
-    case '5':
-      return '0101';
-    case '6':
-      return '0110';
-    case '7':
-      return '0111';
-    case '8':
-      return '1000';
-    case '9':
-      return '1001';
-    case 'a':
-    case 'A':
-      return '1010';
-    case 'b':
-    case 'B':
-      return '1011';
-    case 'c':
-    case 'C':
-      return '1100';
-    case 'd':
-    case 'D':
-      return '1101';
-    case 'e':
-    case 'E':
-      return '1110';
-    case 'f':
-    case 'F':
-      return '1111';
-    default:
-      throw new Error(`Invalid hex digit: ${hexDigit}`);
-  }
+  return parseInt(hexDigit, 16).toString(2).padStart(4, '0');
 };
 
 const hexToBinary = (hex) => {
@@ -64,28 +29,28 @@ const hexToBinary = (hex) => {
 const binaryToDecimal = (binary) => parseInt(binary, 2);
 
 const parseLiteralPacket = (binaryPacket) => {
-  const literalPacket = binaryPacket.substr(6);
-  const groups = _.chunk(literalPacket, 5);
+  const literalPacket = binaryPacket.substr(LITERAL_HEADER_LENGTH);
+  const groups = _.chunk(literalPacket, LITERAL_GROUP_LENGTH);
   const groupCount = _.takeWhile(groups, (group) => group[0] !== '0').length + 1;
   const binaryLiteralNumber = _.chain(groups).take(groupCount).flatMap((group) => _.drop(group, 1).join('')).join('').value();
-  return { literal: binaryToDecimal(binaryLiteralNumber), length: 6 + (groupCount * 5) };
+  return { literal: binaryToDecimal(binaryLiteralNumber), length: LITERAL_HEADER_LENGTH + (groupCount * LITERAL_GROUP_LENGTH) };
 };
 
 const parseLengthType0 = (binaryPacket) => {
-  const restOfPacketLength = binaryToDecimal(binaryPacket.substr(7, 15));
-  let restOfPacket = binaryPacket.substr(7 + 15, restOfPacketLength);
+  const restOfPacketLength = binaryToDecimal(binaryPacket.substr(LENGTH_TYPE_HEADER_LENGTH, LENGTH_TYPE_0_LENGTH));
+  let restOfPacket = binaryPacket.substr(LENGTH_TYPE_HEADER_LENGTH + LENGTH_TYPE_0_LENGTH, restOfPacketLength);
   const packets = [];
   while (restOfPacket.length > 0) {
     const packet = parseBinaryPacket(restOfPacket);
     packets.push(packet);
     restOfPacket = restOfPacket.substr(packet.length);
   }
-  return { lengthTypeId: 0, restOfPacketLength, length: 7 + 15 + restOfPacketLength, packets };
+  return { lengthTypeId: 0, restOfPacketLength, length: LENGTH_TYPE_HEADER_LENGTH + LENGTH_TYPE_0_LENGTH + restOfPacketLength, packets };
 };
 
 const parseLengthType1 = (binaryPacket) => {
-  const numberOfPackets = binaryToDecimal(binaryPacket.substr(7, 11));
-  let restOfPacket = binaryPacket.substr(7 + 11);
+  const numberOfPackets = binaryToDecimal(binaryPacket.substr(LENGTH_TYPE_HEADER_LENGTH, LENGTH_TYPE_1_LENGTH));
+  let restOfPacket = binaryPacket.substr(LENGTH_TYPE_HEADER_LENGTH + LENGTH_TYPE_1_LENGTH);
 
   const packets = [];
 
@@ -94,7 +59,12 @@ const parseLengthType1 = (binaryPacket) => {
     packets.push(packet);
     restOfPacket = restOfPacket.substr(packet.length);
   }
-  return { lengthTypeId: 1, numberOfPackets, length: 7 + 11 + _.sum(packets.map((p) => p.length)), packets };
+  return {
+    lengthTypeId: 1,
+    numberOfPackets,
+    length: LENGTH_TYPE_HEADER_LENGTH + LENGTH_TYPE_1_LENGTH + _.sum(packets.map((p) => p.length)),
+    packets,
+  };
 };
 
 const parseOperatorPacket = (binaryPacket) => {
