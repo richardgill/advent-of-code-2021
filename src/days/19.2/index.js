f359ae3ebc07316481abeeda70e39be4bed7f48c
Richard Gill <richard@rgill.co.uk>
2021-12-19 17:27:47 +0000

    day19

--- src/days/19.2/index.js
+++ src/days/19.2/index.js
@@ -40,15 +40,11 @@ const isDiffEqual = ([ax, ay, az], [bx, by, bz]) => {
 };
 
 const scannerMatches = (scanner1, scanner2) => {
-  console.log('scannerMatches');
   const scanner1Diffs = scannerDifferences(scanner1);
   const scanner2Diffs = scannerDifferences(scanner2);
   const matchedBeacons = scanner1Diffs.map(({ beacon, diffs }) => {
     const matchedBeacon = _.find(scanner2Diffs, (scanner2Diff) => {
-      // console.log('beacon', beacon);
-      // console.log('beacon2', beacon2);
       const diffs2 = scanner2Diff.diffs;
-      // console.log('diffs', diffs, 'diffs2', diffs2);
       return diffs2.filter((diff2) => _.find(diffs, (diff) => isDiffEqual(diff, diff2))).length >= 11;
     });
     if (!matchedBeacon) {
@@ -63,12 +59,6 @@ const doScannersOverlap = (scanner1, scanner2) => {
   return scannerMatches(scanner1, scanner2).length >= 12;
 };
 
-// const calculateDifferences = (x1s, x2s) => {
-//   const differences = x1s.map((x1, index) => x1 - x2s[index]);
-//   if (_.every(differences, (diff) => diff === differences[0])) {
-//     return { add: differences[0] };
-//   }
-// };
 const rearrange = (beacon, rearrange) => {
   return [beacon[rearrange[0]], beacon[rearrange[1]], beacon[rearrange[2]]];
 };
@@ -115,16 +105,12 @@ const REARRANGE_COMBOS = [
 const calcTranslation = (matches) => {
   const scanner1 = matches.map((m) => m.beacon1);
   const scanner2 = matches.map((m) => m.beacon2);
-  // console.log('scanner1', scanner1);
-  // console.log('scanner2', scanner2);
-
   const res = REARRANGE_COMBOS.map((rearrange) => {
     return MULTIPLIER_COMBOS.map((multipliers) => {
       const afterApply = applyTranslation(scanner2, { rearrange, multipliers, adds: [0, 0, 0] });
-      // console.log('afterApply', afterApply);
-      // throw new Error('stop');
       if (
-        areDifferencesSame(scanner1, afterApply, 0) && areDifferencesSame(scanner1, afterApply, 1) &&
+        areDifferencesSame(scanner1, afterApply, 0) &&
+        areDifferencesSame(scanner1, afterApply, 1) &&
         areDifferencesSame(scanner1, afterApply, 2)
       ) {
         return {
@@ -140,57 +126,47 @@ const calcTranslation = (matches) => {
     });
   });
   return _.first(_.compact(_.flatten(res)));
-  // console.log('scanner1col0', getCol(scanner1, 0));
 };
 
-const mergeScanners = (mappedScanners, unmappedScanners) => {
+const mergeScanners = (mappedScanners, unmappedScanners, positions = [[0, 0, 0]]) => {
   console.log('remaining unmapped: ', unmappedScanners.length);
 
-  // console.log('mappedScanners', mappedScanners, 'unmappedScanners', unmappedScanners);
   if (unmappedScanners.length === 0) {
-    return mappedScanners;
+    return positions;
   }
-  // console.log('scanner', scanner);
   let scannerIndex = mappedScanners.length;
   let scanner2Index = -1;
-  console.log('startwhile');
   while (scanner2Index === -1) {
     scannerIndex--;
     const scanner = mappedScanners[scannerIndex];
     scanner2Index = _.findIndex(unmappedScanners, (s2) => doScannersOverlap(scanner, s2));
-    // console.log('scannerIndex', scannerIndex, 'scanner2Index', scanner2Index);
   }
-  console.log('finishwhile');
-  // const scanner2Index = _.findIndex(scanners, (s2) => doScannersOverlap(scanner, s2));
-  // if (scanner2Index === -1) {
-  //   throw new Error('No scanner found');
-  // }
+
   const scanner = mappedScanners[scannerIndex];
   const scanner2 = unmappedScanners[scanner2Index];
-  // console.log('\n\nscanner', scanner);
-  // console.log('scanner2', scanner2);
   const matches = scannerMatches(scanner, scanner2);
-  // console.log('matches', matches);
   const translation = calcTranslation(matches);
-  // console.log('translation', translation);
+  console.log('translation', translation);
   const newPointsToAdd = applyTranslation(scanner2, translation);
-  // console.log('newPointsToAdd', newPointsToAdd);
-  // console.log('back in', _.uniqBy([...newPointsToAdd, ...mergeScanners(newPointsToAdd, withoutIndex(scanners, scanner2Index))], _.isEqual));
-  return mergeScanners([...mappedScanners, newPointsToAdd], withoutIndex(unmappedScanners, scanner2Index));
-  // return _.uniq([...newPointsToAdd, ...mergeScanners(newPointsToAdd, withoutIndex(unmappedScanners, scanner2Index))]);
+  return mergeScanners(
+    [...mappedScanners, newPointsToAdd],
+    withoutIndex(unmappedScanners, scanner2Index),
+    [
+      ...positions,
+      translation.adds,
+    ],
+  );
+};
+
+const manhattanDistance = (a, b) => {
+  return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]) + Math.abs(a[2] - b[2]);
 };
 
 export const solve = (input) => {
   const scanners = parseInput(input);
-  console.log(scanners);
-  const coordinates = mergeScanners([scanners[0]], _.drop(scanners, 1));
-  const uniqueCoordinates = _.uniqBy(_.flatten(coordinates), (x) => x.join(','));
-  console.log('merged', coordinates);
-  console.log('uniq', uniqueCoordinates);
-  // console.log(scannerDifferences(scanners[0]));
-  // console.log(scannerDifferences(scanners[1]));
-  // doScannersOverlap(scanners[0], scanners[1]);
-  return uniqueCoordinates.length;
+  const positions = mergeScanners([scanners[0]], _.drop(scanners, 1));
+  const manhattenDistances = positions.map((x, i) => withoutIndex(positions, i).map((y) => manhattanDistance(x, y)));
+  return _.max(_.flatten(manhattenDistances));
 };
 
 // console.log(solve(readInput('example1.txt')), '\n\n\n');
